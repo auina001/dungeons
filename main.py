f69e4df1a5b45cf2456440ffbe399e0634247eca
auina001 <113507412+auina001@users.noreply.github.com>
2022-09-28 13:00:07 +0200

    commit

--- main.py
+++ main.py
@@ -1,24 +1,67 @@
+from asyncio import shield
 from monster import Monster, Hoved
+import random
 
 level = 0
 
-m = Monster()
-main_hp = 20
+m = Monster
 print("Welcome to dungeons and dragons")
 
-monster1 = m("bill gates", 80, "hack")
-monster2 = m("jeff bezos", 120, "steal")
-monster3 = m("elon musk", 150, "flamethrower")
+monstere = [0, 0, 0]
+
+monstere[0] = m("bill gates", 80, "Jeg kommer til å eliminere deg med min windows AI")
+monstere[1] = m("jeff bezos", 120, "Jeg bruker steroids og kan kjøpe deg")
+monstere[2] = m("elon musk", 150, "Jeg kommer til å sende deg til Mars")
+
+hovedkarakter = Hoved("kjetil", 100)
+
+protection = 0
+extra_damage = 1
+ability_counter = 0
+start_line = False
 
 while True:
-    if (level == 0):
-        print("Jeg er", monster1.navn, "Jeg kommer til å eliminere deg med min windows AI")
+    if (start_line == False):
+        print("Hei, jeg er", monstere[level].navn+".", monstere[level].line)
+        ability_counter = 0
+        start_line = True
+
+    extra_damage = 1
+    protection = 2
+    if (ability_counter < 2):
+        ability = input("Vil du velge en ability (maks to per runde): heal, shield, extra damage: ")
+
+    input("Klikk enter for å angripe")
+
+    if (ability == "heal"):
+        healing = random.randint(10, 20)
+        hovedkarakter.heal(healing)
+        print("Du healet og har nå", hovedkarakter.hp, "liv")
+    elif (ability == "extra damage"):
+        extra_damage = 2
+    elif (ability == "shield"):
+        protection = 1
+    if (ability != ""):
+        ability_counter += 1
+
+    damage = random.randint(15*extra_damage, 25*extra_damage)
+    hoved_damage = random.randint(5*protection, 10*protection)
+    print("Monsteret mister", damage, "liv")
+    print("Du mister", hoved_damage, "liv")
+    monstere[level].lose_hp(damage)
+    hovedkarakter.lose_hp(hoved_damage)
     
-    angrep = input("Velg angrep: attack, heal")
-    if (angrep == "attack"):
-        monster1.lose_hp(20)
-    elif (angrep == "heal"):
-        hovedkarakter.heal(15)
-    if (hovedkarakter.hp <= 0)
-        print("You dead")
+    if (monstere[level].hp <= 0):
+        if (level == 2):
+            print("Nice, du vant!")
+            exit()
+        print(monstere[level].navn, "døde")
+        level += 1
+        start_line = False
+        continue
+
+    if (hovedkarakter.hp <= 0):
+        print("You dead", monstere[level].navn+":", "din noob")
         exit()
+
+    print("Din hp:", hovedkarakter.hp, "Monsterets hp:", monstere[level].hp)
--- monster.py
+++ monster.py
@@ -1,31 +1,20 @@
-main_hp = 100
-
 class Hoved():
-    def __init__(self,navn,hp,ab1,ab2):
-        self.navn=navn
-        self.hp=hp
+    def __init__(self,navn,hp):
+        self.navn = navn
+        self.hp = hp
     
-    def heal(self):
-        self.hp += 10
+    def heal(self, healing):
+        self.hp += healing
 
     def lose_hp(self, damage):
         self.hp -= damage
 
-hovedkarakter = Hoved("kjetil", 100)
 
 class Monster():
-    def __init__(self,navn,hp,ab1):
-        self.navn=navn
-        self.hp=hp
-        self.ab1=ab1
-
-    def abilities(self):
-        if (self.ab1 == "steal"):
-            hovedkarakter.lose_hp(20)
-        elif (self.ab1 == "hack"):
-            hovedkarakter.lose_hp(15)
-        elif (self.ab1 == "flamethrower"):
-            hovedkarakter.lose_hp(25)
+    def __init__(self,navn,hp,line):
+        self.navn = navn
+        self.hp = hp
+        self.line = line
 
     def lose_hp(self, damage):
         self.hp -= damage
